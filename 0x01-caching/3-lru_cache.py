79e2273f2e2d3818ee32b93f17de500e4b06b8c9
AmenMengstu <99494058+AmenMengstu@users.noreply.github.com>
2023-01-25 07:36:10 +0300

    Create 3-lru_cache.py

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" LRUCache that inherits from BaseCaching and is a caching system
+"""
+
+
+from collections import OrderedDict
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ LRUCache defines:
+      - inherits from BaseCaching and is a caching system
+      - where your data are stored (in a dictionary)
+    """
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """assign to the {} self.cache_data the item value for the key
+        """
+        if key and item:
+            if len(self.cache_data) < BaseCaching.MAX_ITEMS:
+                self.cache_data[key] = item
+            else:
+                if key not in self.cache_data:
+                    keys = list(self.cache_data.keys())
+                    print("DISCARD: {}".format(keys[0]))
+                    del self.cache_data[keys[0]]
+                self.cache_data[key] = item
+                self.cache_data.move_to_end(key)
+
+    def get(self, key):
+        """return value in self.cache_data linked to key
+        """
+        if not key or not self.cache_data.get(key):
+            return None
+        self.cache_data.move_to_end(key)
+        return self.cache_data.get(key)
