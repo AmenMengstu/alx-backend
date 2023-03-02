3d41b63a821ec2d525e0d6600256eefde08c522f
AmenMengstu <99494058+AmenMengstu@users.noreply.github.com>
2023-03-01 23:07:38 -0800

    Update 0-redis_client.js

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,11 +1,12 @@
-import redis from 'redis';
+#!/usr/bin/yarn dev
+import { createClient } from 'redis';
 
-//this creates a new client
-const client = redis.createClient();
-//By default redis.createClient() will use 127.0.0.1 and port 6379
+const client = createClient();
 
-//listen for the connect event to see whether we successfully connected to the redis-server
-client.on('connect', () => console.log('Redis client connected to the server'));
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
 
-// listen for the error event tocheck if we failed to connect to the redis-server
-client.on('error', (err) => console.error(`Redis client not connected to the server: ${err.message}`));
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
