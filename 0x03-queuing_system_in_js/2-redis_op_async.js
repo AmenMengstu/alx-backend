dc101ad57b2013b29063d9e4b574f9aca2b88e18
AmenMengstu <99494058+AmenMengstu@users.noreply.github.com>
2023-03-01 23:08:40 -0800

    Update 2-redis_op_async.js

--- 0x03-queuing_system_in_js/2-redis_op_async.js
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -1,27 +1,28 @@
-import redis from 'redis';
-const { promisify } = require('util')
+#!/usr/bin/yarn dev
+import { promisify } from 'util';
+import { createClient, print } from 'redis';
 
-// this creates a new client
-const client = redis.createClient(); 
-// By default redis.createClient() will use 127.0.0.1 and port 6379
+const client = createClient();
 
-const asyncGet = promisify(client.get).bind(client);
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
 
-// listen for the connect event to see whether we successfully connected to the redis-server
-client.on('connect', () => console.log('Redis client connected to the server'));
+const setNewSchool = (schoolName, value) => {
+  client.SET(schoolName, value, print);
+};
 
-// listen for the error event tocheck if we failed to connect to the redis-server
-client.on('error', (err) => console.error(`Redis client not connected to the server: ${err.message}`));
+const displaySchoolValue = async (schoolName) => {
+  console.log(await promisify(client.GET).bind(client)(schoolName));
+};
 
-function setNewSchool(schoolName, value) {
-    // redis.print prints “Reply: OK” to the console saying that redis saved the value
-    client.set(schoolName, value, redis.print);
+async function main() {
+  await displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
 }
 
-async function displaySchoolValue(schoolName) {
-    console.log(await asyncGet(schoolName));
-}
-
-displaySchoolValue('Holberton');
-setNewSchool('HolbertonSanFrancisco', '100');
-displaySchoolValue('HolbertonSanFrancisco');
+client.on('connect', async () => {
+  console.log('Redis client connected to the server');
+  await main();
+});
